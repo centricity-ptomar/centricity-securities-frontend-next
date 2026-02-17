12031f02f565bd753b6d35904a75515b7ecf5960
Pranav Tomar <ptomar@centricity.co.in>
2026-02-17 12:01:27 +0530

    deploy added for static website

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: 'export',
+  // If deploying to a subpath, set basePath and assetPrefix:
+  basePath: '/centricity-securities-frontend-next', // Replace with your repo name (must start with a slash)
+  assetPrefix: '/centricity-securities-frontend-next/', // Replace with your repo name (must end with a slash)
+  // Optional: Configure images if needed for static export
+  images: {
+    unoptimized: true, // Disable Next.js image optimization as it requires a server
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
